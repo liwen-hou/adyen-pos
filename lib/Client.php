88e957b3f62ccfb80e8fc7cb3c53bb0f10cab42a
Liwen Hou <liwen.hou@adyen.com>
2019-04-04 14:43:22 +0800

    test timestamp

--- lib/Client.php
+++ lib/Client.php
@@ -9,76 +9,74 @@ class Client
   {
     try{
       // Authentication
-      // $authentication = Config::getAuthentication();
+      $authentication = Config::getAuthentication();
 
       // Generate url
-      // $url = Config::getTerminalUrl();
+      $url = Config::getTerminalUrl();
       $date = new DateTime();
-      // $date = strval($date);
-      // $serviceID =  strval(mt_rand(100000,999999));
       // Generate data
-      echo $date;
-      // $request = array(
-      //   "SaleToPOIRequest" => array(
-      //     "MessageHeader" => array(
-      //       "ProtocolVersion" => "3.0",
-      //       "MessageClass" => "Service",
-      //       "MessageCategory" => "Payment",
-      //       "MessageType" => "Request",
-      //       "ServiceID" => $serviceID,
-      //       "SaleID" => "liwenShopID001",
-      //       "POIID" => "e285-401552056"
-      //     ),
-      //     "PaymentRequest" => array(
-      //       "SaleData" => array(
-      //         "SaleTransactionID" => array(
-      //           "TransactionID" => "27908",
-      //           "TimeStamp" => $date
-      //         )
-      //       ),
-      //       "PaymentTransaction" => array(
-      //         "AmountsReq" => array(
-      //           "Currency" => "EUR",
-      //           "RequestedAmount" => $_POST["paymentAmount"]
-      //         )
-      //       )
-      //     )
-      //   )
-      // );
-      //
-      // $data = json_encode($request);
-      // //  Initiate curl
-      // $curlAPICall = curl_init();
-      //
-      // // Set to POST
-      // curl_setopt($curlAPICall, CURLOPT_CUSTOMREQUEST, "POST");
-      //
-      // // Will return the response, if false it print the response
-      // curl_setopt($curlAPICall, CURLOPT_RETURNTRANSFER, true);
-      //
-      // // Add JSON message
-      // curl_setopt($curlAPICall, CURLOPT_POSTFIELDS, $data);
-      //
-      // // Set the url
-      // curl_setopt($curlAPICall, CURLOPT_URL, $url);
-      //
-      // // Api key
-      // curl_setopt($curlAPICall, CURLOPT_HTTPHEADER,
-      //   array(
-      //     "X-Api-Key: " . $authentication['checkoutAPIkey'],
-      //     "Content-Type: application/json",
-      //     "Content-Length: " . strlen($data)
-      //   )
-      // );
-      // // Execute
-      // $result = curl_exec($curlAPICall);
-      // // Error Check
-      // if ($result === false){
-      //   throw new Exception(curl_error($curlAPICall), curl_errno($curlAPICall));
-      // }
-      //
-      // // Closing
-      // curl_close($curlAPICall);
+      $serviceID = strval(mt_rand(1000000,9999999))
+      $request = array(
+        "SaleToPOIRequest" => array(
+          "MessageHeader" => array(
+            "ProtocolVersion" => "3.0",
+            "MessageClass" => "Service",
+            "MessageCategory" => "Payment",
+            "MessageType" => "Request",
+            "ServiceID" => $serviceID,
+            "SaleID" => "liwenShopID001",
+            "POIID" => "e285-401552056"
+          ),
+          "PaymentRequest" => array(
+            "SaleData" => array(
+              "SaleTransactionID" => array(
+                "TransactionID" => "27908",
+                "TimeStamp" => "2019-04-04T01:44:13.573Z"
+              )
+            ),
+            "PaymentTransaction" => array(
+              "AmountsReq" => array(
+                "Currency" => "EUR",
+                "RequestedAmount" => 100.99
+              )
+            )
+          )
+        )
+      );
+
+      $data = json_encode($request);
+      //  Initiate curl
+      $curlAPICall = curl_init();
+
+      // Set to POST
+      curl_setopt($curlAPICall, CURLOPT_CUSTOMREQUEST, "POST");
+
+      // Will return the response, if false it print the response
+      curl_setopt($curlAPICall, CURLOPT_RETURNTRANSFER, true);
+
+      // Add JSON message
+      curl_setopt($curlAPICall, CURLOPT_POSTFIELDS, $data);
+
+      // Set the url
+      curl_setopt($curlAPICall, CURLOPT_URL, $url);
+
+      // Api key
+      curl_setopt($curlAPICall, CURLOPT_HTTPHEADER,
+        array(
+          "X-Api-Key: " . $authentication['checkoutAPIkey'],
+          "Content-Type: application/json",
+          "Content-Length: " . strlen($data)
+        )
+      );
+      // Execute
+      $result = curl_exec($curlAPICall);
+      // Error Check
+      if ($result === false){
+        throw new Exception(curl_error($curlAPICall), curl_errno($curlAPICall));
+      }
+
+      // Closing
+      curl_close($curlAPICall);
     } catch (Exception $e) {
       trigger_error(sprintf(
         'API call failed with error #%d, %s', $e->getCode(), $e->getMessage()
@@ -86,6 +84,7 @@ class Client
     }
 
     // When this file gets called by javascript or another language, it will respond with a json object
+    return $result;
   }
 
 
@@ -93,9 +92,9 @@ class Client
 
 if(isset($_POST['submit']))
 {
-  $date = new DateTime();
-  echo $date;
-  // $result = json_decode($result, true);
+  $result = Client::doPostRequest();
+  echo $result;
+  $result = json_decode($result, true);
 
 }
 
