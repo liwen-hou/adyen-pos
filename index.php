8850237c3cf45891bd16f1dc921cac1b3be28281
Liwen Hou <liwen.hou@adyen.com>
2019-04-04 11:01:59 +0800

    change form

--- index.php
+++ index.php
@@ -69,38 +69,12 @@ date_default_timezone_set("Europe/Amsterdam");
           <div class="card">
             <div class="card-body">
               <form class="needs-validation" novalidate="">
-                <div class="row">
-                  <div class="col-md-6 mb-3">
-                    <label for="firstName">First name</label>
-                    <input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
-                    <div class="invalid-feedback">
-                      Valid first name is required.
-                    </div>
-                  </div>
-                  <div class="col-md-6 mb-3">
-                    <label for="lastName">Last name</label>
-                    <input type="text" class="form-control" id="lastName" placeholder="" value="" required="">
-                    <div class="invalid-feedback">
-                      Valid last name is required.
-                    </div>
-                  </div>
-                </div>
-
-                <div class="mb-3">
-                  <label for="email">Email</label>
-                  <input type="email" class="form-control" id="email" placeholder="you@example.com">
-                  <div class="invalid-feedback">
-                    Please enter a valid email address for shipping updates.
-                  </div>
-                </div>
-
-
+                <p class="card-text">You will pay 100.99 EUR to Adyen.</p>
 
                 <hr class="mb-4">
 
-                <button class="btn btn-primary btn-lg btn-block" type="button">Continue to checkout</button>
+                <button class="btn btn-primary btn-lg btn-block" type="button">Pay Now</button>
               </form>
-              <p class="card-text">This will bring you to the payment details page.</p>
             </div>
           </div>
         </div>
