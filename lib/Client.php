335479f8c00be748ece2a343abfc75be4961bd30
Liwen Hou <liwen.hou@adyen.com>
2019-04-05 16:09:39 +0800

    print response result

--- lib/Client.php
+++ lib/Client.php
@@ -92,66 +92,67 @@ class Client
 if(isset($_POST['submit']))
 {
   $result = Client::doPostRequest();
+  echo $result;
   $result = json_decode($result, true);
-  if ($result["SaleToPOIResponse"]["PaymentResponse"]["Response"]["Result"] == "Success"){
-    echo '<!DOCTYPE html>
-    <html class="html">
-    <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-      <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
-      <meta name="robots" content="noindex"/>
-      <title>Adyen Checkout</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-      <link rel="stylesheet" type="text/css" href="../assets/css/main.css">
-
-    </head>
-
-    <body>
-
-      <div class="container" id="paymentResult">
-        <div class="py-5 text-center">
-          <img class="d-block mx-auto mb-4" src="../assets/img/shopping.png" alt="" width="100" height="100">
-          <h2 class="heading">Payment Successful! Your Order is On the Way!</h2>
-        </div>
-      </div>
-
-      <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-
-    </body>
-    </html>';
-  } else {
-    echo '<!DOCTYPE html>
-    <html class="html">
-    <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-      <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
-      <meta name="robots" content="noindex"/>
-      <title>Adyen Checkout</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-      <link rel="stylesheet" type="text/css" href="../assets/css/main.css">
-
-    </head>
-
-    <body>
-
-      <div class="container" id="paymentResult">
-        <div class="py-5 text-center">
-          <img class="d-block mx-auto mb-4" src="../assets/img/error.png" alt="" width="100" height="100">
-          <h2 class="heading">Oops! Something Went Wrong..</h2>
-        </div>
-      </div>
-
-      <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-
-    </body>
-    </html>';
-  }
+  // if ($result["SaleToPOIResponse"]["PaymentResponse"]["Response"]["Result"] == "Success"){
+  //   echo '<!DOCTYPE html>
+  //   <html class="html">
+  //   <head>
+  //     <meta charset="utf-8">
+  //     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  //     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+  //     <meta name="robots" content="noindex"/>
+  //     <title>Adyen Checkout</title>
+  //     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  //     <link rel="stylesheet" type="text/css" href="../assets/css/main.css">
+  //
+  //   </head>
+  //
+  //   <body>
+  //
+  //     <div class="container" id="paymentResult">
+  //       <div class="py-5 text-center">
+  //         <img class="d-block mx-auto mb-4" src="../assets/img/shopping.png" alt="" width="100" height="100">
+  //         <h2 class="heading">Payment Successful! Your Order is On the Way!</h2>
+  //       </div>
+  //     </div>
+  //
+  //     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  //     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  //     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  //
+  //   </body>
+  //   </html>';
+  // } else {
+  //   echo '<!DOCTYPE html>
+  //   <html class="html">
+  //   <head>
+  //     <meta charset="utf-8">
+  //     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  //     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+  //     <meta name="robots" content="noindex"/>
+  //     <title>Adyen Checkout</title>
+  //     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  //     <link rel="stylesheet" type="text/css" href="../assets/css/main.css">
+  //
+  //   </head>
+  //
+  //   <body>
+  //
+  //     <div class="container" id="paymentResult">
+  //       <div class="py-5 text-center">
+  //         <img class="d-block mx-auto mb-4" src="../assets/img/error.png" alt="" width="100" height="100">
+  //         <h2 class="heading">Oops! Something Went Wrong..</h2>
+  //       </div>
+  //     </div>
+  //
+  //     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  //     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  //     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  //
+  //   </body>
+  //   </html>';
+  // }
 
 }
 
